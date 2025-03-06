e2f7a3883fd170af39909e4bfbc9beff25be8b10
alucard233 <crmartinezhdz@gmail.com>
2025-03-06 00:13:24 -0600

    Update config.py

--- CURP/CURP/app/config.py
+++ CURP/CURP/app/config.py
@@ -1,4 +1,4 @@
 # config.py
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123456@localhost:5432/mexico'  # Replace with your PostgreSQL credentials
+    SQLALCHEMY_DATABASE_URI = 'postgresql://super:hWH9GUx88rEDt9@super.postgres.pythonanywhere-services.com/mexico'  # Replace with your PostgreSQL credentials
     SQLALCHEMY_TRACK_MODIFICATIONS = False
